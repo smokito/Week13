a68e038716670f24cd046fecc3ddb9c1d4594ce4
smokito <anna.aksiuk.aa@gmail.com>
2022-08-06 21:18:53 +0200

    created pages button and calculator

---
+++ button.js
@@ -0,0 +1,4 @@
+function askName (){
+    let name = prompt("Hello, what is your name?");
+    alert(`Hello, ${name}`);
+}
---
+++ calculator.js
@@ -0,0 +1,31 @@
+/*let name = prompt("Hello, what ist your name?");
+alert(`Hello ${name}`);*/
+
+
+function addNumbers ( ) {
+    let firstcount = prompt ("Enter the first number");
+    let secondcount = prompt ("Enter the second number");
+    alert(+firstcount + +secondcount);
+    
+}
+
+function subtractNumbers () {
+    let firstcount = prompt ("Enter the first number");
+    let secondcount = prompt ("Enter the second number");
+    alert(+firstcount - +secondcount);
+}
+
+function multiplyNumbers () {
+    let firstcount = prompt ("Enter the first number");
+    let secondcount = prompt ("Enter the second number");
+    alert(+firstcount * +secondcount);
+}
+
+function divideNumbers () {
+    let firstcount = prompt ("Enter the first number");
+    let secondcount = prompt ("Enter the second number");
+    alert(+firstcount / +secondcount);
+}
+
+
+
